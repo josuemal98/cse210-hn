09b4f8c01b41aa32df7bc0710839e453bc6f93a1
josuemal98 <jmaldonadofuenntes@byupathway.edu>
2026-05-09 10:14:14 -0600

    I made changes/ test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!!!");
+        Console.WriteLine("Hello Sandbox World!");
     }
 }
+
+// This is just a test for the class.
