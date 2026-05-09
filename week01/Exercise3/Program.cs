f16859610f30b31eb9b2323bb1b1e1a254eaa399
josuemal98 <jmaldonadofuenntes@byupathway.edu>
2026-05-09 12:38:03 -0600

    no kidding, it is fixed, i think

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,17 +2,29 @@ using System;
 
 class Program
 {
-    
     static void Main(string[] args)
     {
+    
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
             Random randomgenerator = new Random();
-            int magicnumber = randomgenerator.Next(1, 100);
+            int magicnumber = randomgenerator.Next(1, 101);
 
             int guess = 0;
+         
+            int attempts = 0;
+
             while (guess != magicnumber)
             {
                 Console.WriteLine("Guess the magic number between 1 and 100:");
+                
+                
                 guess = int.Parse(Console.ReadLine());
+
+                
+                attempts = attempts + 1;
 
                 if (guess < magicnumber)
                 {
@@ -25,9 +37,15 @@ class Program
                 else
                 {
                     Console.WriteLine("Congratulations! You guessed the magic number!");
+                    
+                    Console.WriteLine("It took you " + attempts + " guesses.");
                 }
             }
-     }
-}
 
+    
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            playAgain = Console.ReadLine();
+        }
+    }
+}
 
