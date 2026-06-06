07c86e8d108ce0ddb53a46e0152f966cfcdd7f5d
josuemal98 <jmaldonadofuenntes@byupathway.edu>
2026-06-06 14:48:40 -0600

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,17 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Josue Maldonado", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Daniel Fuentes", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Armando Paredes", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
